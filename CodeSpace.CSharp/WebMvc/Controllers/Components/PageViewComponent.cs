84cbe9c358234f69fbda22aedbb09f23f3809d3e
杨惠超 <472189065@qq.com>
2019-09-07 22:18:05 +0800

    分页组件

---
+++ CodeSpace.CSharp/WebMvc/Controllers/Components/PageViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Controllers.Component
+{
+    public class PageViewComponent:ViewComponent
+    {
+
+        protected readonly IHostingEnvironment Env;
+        //public PageViewComponent()
+        //{
+            
+        //}
+        public PageViewComponent(IHostingEnvironment env)
+        {
+            Env = env;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string href)
+        {
+            if (href.IndexOf("?") > -1)
+            {
+                ViewData["Page_BaseHref"] = href + "&pageIndex=";
+            }
+            else {
+                ViewData["Page_BaseHref"] = href + "?pageIndex=";
+            }
+            
+            return View();
+        }
+    }
+}
