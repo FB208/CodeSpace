2e49dc603d2819e7c0bd6b75fea8a185dd600100
yhc in cbhb <472189065@qq.com>
2019-09-09 09:32:58 +0800

    补充提交ini文件

--- CodeSpace.CSharp/FBCodeProduce/FBCodeProduce/Properties/Resources.Designer.cs
+++ CodeSpace.CSharp/FBCodeProduce/FBCodeProduce/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FBCodeProduce.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -47,7 +47,7 @@ namespace FBCodeProduce.Properties {
         }
         
         /// <summary>
-        ///   使用此强类型资源类，为所有资源查找
+        ///   重写当前线程的 CurrentUICulture 属性
         ///   重写当前线程的 CurrentUICulture 属性。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
@@ -61,7 +61,7 @@ namespace FBCodeProduce.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap _1 {
             get {
@@ -95,7 +95,8 @@ namespace FBCodeProduce.Properties {
         ///tinyint=System.Byte 
         ///uniqueidentifier=System.Guid 
         ///varbinary=System.Byte[] 
-        ///varchar=String        /// [字符串的其余部分被截断]&quot;; 的本地化字符串。
+        ///varchar=String
+        /// [字符串的其余部分被截断]&quot;; 的本地化字符串。
         /// </summary>
         internal static string Config {
             get {
@@ -104,7 +105,7 @@ namespace FBCodeProduce.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Icon icon {
             get {
@@ -114,7 +115,7 @@ namespace FBCodeProduce.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap line {
             get {
@@ -124,7 +125,7 @@ namespace FBCodeProduce.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap setting {
             get {
