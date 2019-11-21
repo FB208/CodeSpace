8ce86c3fbef8dedaf56dc23b8bab7ceb4efc8d5e
FB208 <yanghuichaofb@gmail.com>
2019-11-21 17:25:37 +0800

    删除单客户端通讯，精简代码

--- CodeSpace.CSharp/TCP.Client/TCP.Client/TCP/ListenerView.cs
+++ CodeSpace.CSharp/TCP.Client/TCP.Client/TCP/ListenerView.cs
@@ -284,46 +284,6 @@ namespace TCP.Client.TCP
             tb_console.AppendText($"{text}\r\n");
         }
 
-        /// <summary>
-        /// 单客户端通讯
-        /// </summary>
-        /// <param name="tcpclient"></param>
-        /// <param name="ipadd"></param>
-        /// <param name="myD"></param>
-        private void HandleClient(TcpClient tcpclient, string ipadd, Delegate myD)
-        {
-
-            lock (tcpclient)
-            {
-                if (tcpclient == null)
-                {
-                    return;
-                }
-
-                // Buffer for reading data
-                Byte[] bytes = new Byte[1024 * 100];
-                String data = null;
-
-                // Enter the listening loop.
-                while (tcpclient.Connected)
-                {
-                    data = null;
-                    NetworkStream stream = tcpclient.GetStream();
-                    int i;
-                    i = stream.Read(bytes, 0, bytes.Length);
-                    if (i != 0)
-                    {
-                        data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
-
-                        byte[] msg = System.Text.Encoding.UTF8.GetBytes($"return:{data}");
-                        stream.Write(msg, 0, msg.Length);
-                        Invoke(myD, data);
-                        //tb_console.AppendText($"{data}\r\n");
-                    }
-                    //tcpclient.Close();
-                }
-            }
-        }
 
     }
 }
