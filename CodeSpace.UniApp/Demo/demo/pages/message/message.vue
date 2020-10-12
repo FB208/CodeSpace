c19c4d9e9ea90f0228a034794e80fc8d93b924d9
杨惠超 <fb208@outlook.com>
2020-10-12 08:29:49 +0800

    新增消息页

---
+++ CodeSpace.UniApp/Demo/demo/pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		message
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
